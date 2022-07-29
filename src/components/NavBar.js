3404ba10b79b878f080f2eb8e04c51e17c363fed
Tomás Pica <tomaspica31@gmail.com>
2022-07-28 21:19:18 -0300

    nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import '../App.css'
+
+function NavBar() {
+    return(
+        <header className='NavBar'>
+        <div>
+            <h1 class='title'>Boxeo</h1>
+            <ul className= 'uls'>
+                <li>Log In</li>
+                <li>Inicio</li>
+                <li>Carrito</li>
+            </ul>
+        </div>
+    </header>
+    )
+    
+}
+
+export default NavBar;
+
